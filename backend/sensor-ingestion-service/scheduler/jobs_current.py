705c3a7dbbe08fc35882f5fa07ce39e13041f082
NemanjaB01 <nmnjblt@email.com>
2025-12-13 16:38:47 +0100

    fix: added email field

--- backend/sensor-ingestion-service/scheduler/jobs_current.py
+++ backend/sensor-ingestion-service/scheduler/jobs_current.py
@@ -19,7 +19,8 @@ def fetch_current_weather_for_all():
 
     for user in users:
         user_id = user["id"]
+        email = user["email"]
         farms = user.get("farms", [])
 
         for farm in farms:
-            fetch_and_publish_for_farm(user_id, farm, ForecastType.CURRENT)
+            fetch_and_publish_for_farm(user_id, email, farm, ForecastType.CURRENT)
--- backend/sensor-ingestion-service/scheduler/jobs_daily.py
+++ backend/sensor-ingestion-service/scheduler/jobs_daily.py
@@ -19,7 +19,8 @@ def fetch_daily_weather_for_all():
 
     for user in users:
         user_id = user["id"]
+        email = user["email"]
         farms = user.get("farms", [])
 
         for farm in farms:
-            fetch_and_publish_for_farm(user_id, farm, ForecastType.DAILY)
+            fetch_and_publish_for_farm(user_id, email, farm, ForecastType.DAILY)

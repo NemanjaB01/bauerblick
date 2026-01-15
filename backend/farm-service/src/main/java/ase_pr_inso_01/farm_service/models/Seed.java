7a2e00a2d967896761320ea6d37620f2170fcd60
NemanjaB01 <nmnjblt@email.com>
2026-01-15 22:18:51 +0100

    fix: seeds db connected again

--- backend/farm-service/src/main/java/ase_pr_inso_01/farm_service/models/Seed.java
+++ backend/farm-service/src/main/java/ase_pr_inso_01/farm_service/models/Seed.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-@Document(collection = "seeds")
+@Document(collection = "seed")
 @Getter
 @Setter
 public class Seed {
